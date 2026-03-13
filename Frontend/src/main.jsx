773ce5a70b24b5a21e30897c15cc384a62c05d2f
Micky <eaditingsadvideo@gmail.com>
2026-03-13 16:20:34 +0530

    auto theam fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,15 +3,31 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+
 import "./styles/index.css";
 // import "./styles/auth.css";
 
 import { setupInterceptors } from "./api/setupInterceptors";
+
 setupInterceptors();
 
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+/* SYSTEM THEME AUTO */
 
-document.documentElement.dataset.theme = prefersDark ? "dark" : "light";
+const applySystemTheme = () => {
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  document.documentElement.dataset.theme = isDark ? "dark" : "light";
+};
+
+applySystemTheme();
+
+/* LISTEN SYSTEM THEME CHANGE */
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", applySystemTheme);
+
+/* REACT APP */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
