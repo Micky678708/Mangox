f354dbf69d2a393827c663a385fd9c5ec581be64
Micky <eaditingsadvideo@gmail.com>
2026-03-14 12:30:11 +0530

    fix theme error

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,17 +7,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import "./styles/index.css";
 
 import { setupInterceptors } from "./api/setupInterceptors";
-
 setupInterceptors();
-
-/* -------- THEME SYSTEM (STABLE) -------- */
-
-const media = window.matchMedia("(prefers-color-scheme: dark)");
-
-media.addEventListener("change", (e) => {
-const newTheme = e.matches ? "dark" : "light";
-document.documentElement.dataset.theme = newTheme;
-});
 /* ---------------- REACT APP ---------------- */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
